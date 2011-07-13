e37ce9ece47ef6139d3921355414d038c4adbdaf
JOGAMP DEV TEAM <jogl@jogamp.org>
2011-07-13 05:22:05 +0200

    sync changes

--- planet2/update-stream.sh
+++ planet2/update-stream.sh
@@ -1,3 +1,2 @@
 cd /home/jogl/jogamp.org/planet2
-/opt-linux-x86/jdk1.6.0_21/bin/java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar /srv/www/jogamp.org/planet2/config/stream-config.xml
-
+/opt-linux-x86_64/j2se6/bin/java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar /srv/www/jogamp.org/planet2/config/stream-config.xml
