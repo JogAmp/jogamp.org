cf105c58622f37789ce5f8667578834ec49b6320
JOGAMP WEB TEAM <jogamp_web@jogamp.org>
2013-06-19 21:15:49 +0200

    update-planet* use jre7

--- planet2/update-planet.sh
+++ planet2/update-planet.sh
@@ -3,7 +3,8 @@
 PLANET2=/srv/www/jogamp.org/planet2
 cd $PLANET2
 
-export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
+export JRE_HOME=/opt-linux-x86_64/jre7
+# export JRE_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 
-$JAVA_HOME/bin/java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar $PLANET2/config/planet-config.xml
+$JRE_HOME/bin/java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar $PLANET2/config/planet-config.xml
 
