bbf9dd07386e0cc5163cab5909d43207461e16ff
Sven Göthel <sgothel@jausoft.com>
2025-08-31 21:16:01 +0200

    Use Java11 for planet2

--- planet2/update-planet.sh
+++ planet2/update-planet.sh
@@ -4,7 +4,8 @@ PLANET2=/srv/www/jogamp.org/planet2
 cd $PLANET2
 
 # export JRE_HOME=/opt-linux-x86_64/jre7
-export JRE_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+export JRE_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+#export JRE_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 
 $JRE_HOME/bin/java -jar HungryHarry-SNAPSHOT-jar-with-dependencies.jar $PLANET2/config/planet-config.xml
 
